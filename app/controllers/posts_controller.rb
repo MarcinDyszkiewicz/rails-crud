4ea0ea95c66d668ed8b502e7d5d52c68b8884e00
marcin <mdyszkiewicz@9bits.pl>
2019-02-08 12:16:48 +0100

    Added update, show, edit functions and simple form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,39 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Record added!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Oops problem"
+      render action: "new"
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Record updated!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Oops problem"
+      render action: "edit"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body, :published)
+  end
+
 end
